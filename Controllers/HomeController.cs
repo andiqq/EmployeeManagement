8c20af9a8c0072ef03415b1dfa87f1f442568310
Dr. Andreas Plagens <andreas@plagens.eu>
2021-01-17 09:47:30 +0100

    Starting ASP.NET Tutorial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+
+namespace EmployeeManagement.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+
+        public ViewResult Details()
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                PageTitle = "Employee Details"
+            };
+
+            return View(homeDetailsViewModel);
+        }
+
+
+       
+    }
+
+   
+}
